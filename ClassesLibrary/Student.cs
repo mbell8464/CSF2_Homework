15cb35382e5a11bcbc3968183feeeaf65db68fee
mbell8464 <michaelb2127@charter.net>
2020-11-05 23:46:13 -0600

    Student

---
+++ ClassesLibrary/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student
+    {
+        private float _gpa;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set;  }
+
+
+        public float GPA
+        {
+            get { return _gpa; }
+            set
+            {
+                _gpa = value >= 0 && value <= 4 ? value : 0;
+            }
+        }
+
+
+        public Student(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            GPA = gpa;
+
+        }//end ctor
+
+        public override string ToString()
+        {
+            return string.Format("First Name: {0}\nLast Name: {1}\nID: {2}\nGPA: {3:n1}\n", FirstName, LastName, Id, GPA);
+        }
+    }
+}
---
+++ TesterProgram/TestHarness.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesLibrary;
+
+namespace TesterProgram
+{
+    class TestHarness
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****STUDENT****");
+
+            Student s1 = new Student("Spongebob", "Squarepants", "256", 2.8f);
+            Student s2 = new Student("Michael", "Scott", "659", 0.19f);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
