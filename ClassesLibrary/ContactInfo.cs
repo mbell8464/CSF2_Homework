2131454809781179f7d0858f201918984f00b991
mbell8464 <michaelb2127@charter.net>
2020-11-06 00:55:32 -0600

    Contact Info HW

---
+++ ClassesLibrary/ContactInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class ContactInfo
+    {
+        public string StreetAddress { get; set; }
+        public string  City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+
+        public ContactInfo() { }
+
+        public ContactInfo(string streetAddress, string city, string state, string zip, string phone, string email)
+        {
+            StreetAddress = streetAddress;
+            City = city;
+            State = state;
+            Zip = zip;
+            Phone = phone;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Address: {0}\nCity: {1}\nState: {2}\nZip: {3}\nPhone: {4}\nEmail: {5}\n",
+                StreetAddress,
+                City,
+                State,
+                Zip,
+                Phone,
+                Email);
+        }
+    }
+}
--- TesterProgram/TestHarness.cs
+++ TesterProgram/TestHarness.cs
@@ -11,6 +11,7 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
+            //*****STUDENT*****
             Console.WriteLine("****STUDENT****");
 
             Student s1 = new Student("Spongebob", "Squarepants", "256", 2.8f);
@@ -20,6 +21,7 @@ namespace TesterProgram
             Console.WriteLine(s2);
 
 
+            //*****VEHICLE*****
             Console.WriteLine("****VEHICLE****");
 
             Vehicle v1 = new Vehicle("Kia", "Optima", 2015, 3005f);
@@ -29,6 +31,7 @@ namespace TesterProgram
             Console.WriteLine(v2);
 
 
+            //*****LOGIN*****
             Console.WriteLine("****LOGIN****");
 
             Login l1 = new Login("MScott1", "WorldsBestBoss");
@@ -36,6 +39,48 @@ namespace TesterProgram
 
             Console.WriteLine(l1);
             Console.WriteLine(l2);
+
+
+            //*****CONTACT INFO*****
+            Console.WriteLine("****CONTACTINFO****");
+
+            ContactInfo c1 = new ContactInfo("123 Main St.", "Springfield", "Mars", "12345", "1234567890", "email@email.email");
+            ContactInfo c2 = new ContactInfo("987 Secondary Dr.", "Boardfield", "Jupitar", "98765", "9876543210", "example@example.email");
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+
+
+            //*****CUSTOMER*****
+
+
+
+            //*****CREDIT CARD ACCOUNT*****
+
+
+
+            //*****BOOK*****
+
+
+
+            //*****LIBRARY*****
+
+
+
+            //*****SONG*****
+
+
+
+            //*****ARTIST*****
+
+
+
+            //*****MOTOR HOME - extend VEHICLE*****
+
+
+
+            //*****TRUCK - extend VEHICLE*****
+
         }
     }
 }
